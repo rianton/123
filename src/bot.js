e5e9af5139a66bc96c170a9e6879cc778299062c
anto <32399451+rianton@users.noreply.github.com>
2018-05-01 20:18:45 +0700

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -30,8 +30,8 @@ const auth = {
 	password: ''
 }
 
-//let client =  new LineConnect();
-let client =  new LineConnect(auth);
+let client =  new LineConnect();
+//let client =  new LineConnect(auth);
 
 client.startx().then(async (res) => {
 	while(true) {
